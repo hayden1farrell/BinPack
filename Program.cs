97a34475a8baf0d78bb29e21e49e997de2eb39d6
hayden1farrell <90006718+hayden1farrell@users.noreply.github.com>
2022-01-17 07:28:48 +0000

    Correct file

--- Program.cs
+++ Program.cs
@@ -1,76 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
+using System.Linq;
+using System.Text;
 
-namespace BinPack
+namespace BinPackTell
 {
+    struct Bin{
+        public int Space;
+        public StringBuilder Items;
+    }
     class Program
     {
-
-        static void BinPack(int maxBinSize, int[] packets){
-            // Dictionary where key is the number of spaces left and values are the number of bins with that amount of space
-            Dictionary<int, int> binMap = new Dictionary<int, int>(); // allows memory use to be really low
-            int spaceNeeded = 0;
-            int neededSpace = 0;
-            bool found  = false;
-            int checkBin = 0; int NewSize = 0;
-            // Variable definment done above so only done once
-
-            Console.WriteLine("Press any key to start...");
-            Console.ReadKey();
-            Console.WriteLine("Algo has started");
-
-            for(int i = 0; i <= maxBinSize; i++)
-                binMap.Add(i, 0);
-
-            for(int current = 0; current < packets.Length; current++){
-                spaceNeeded = packets[current];
-                neededSpace = maxBinSize - spaceNeeded;
-
-                if(spaceNeeded == maxBinSize)
-                    binMap[0] += 1;
-                else{
-                    found = false;
-                    for(checkBin = spaceNeeded; checkBin < binMap.Count; checkBin++){
-                        if(binMap[checkBin] != 0){
-                            NewSize = checkBin - spaceNeeded;
-                            binMap[checkBin] -= 1;
-                            binMap[NewSize] += 1;
-
-                            found = true;
-                            break;
-                        }
-                    }
-                    if(found == false)
-                        binMap[neededSpace] += 1;
-                }
-            }
-            Console.WriteLine("Complete");
-
-            DisplayBins(binMap);
-        }
-
-        static void DisplayBins( Dictionary<int, int> binMap){
-            long total = 0;
-            foreach (var item in binMap){
-                Console.WriteLine($"KEY (space left): {item.Key} VALUE (number of bins): {item.Value}");
-                total += item.Value;
-            }
-            Console.WriteLine("Bins used: " + total);
-        }
         static void Main(string[] args)
         {
-
             //2,3,4,1,3,2,3,3,2
             Console.WriteLine("Enter Max bin size");
             int maxBinSize = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("Enter packet string");
+            Console.WriteLine("Enter packet string");
+            string packetData = Console.ReadLine();
+            int[] packets = packetData.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
 
-            int[] packets = ReadFileData();
+            packets = ReadFileData();
 
-            Console.WriteLine(packets.Max());
+            Console.WriteLine("Number of items: " + packets.Length);
+
             if(packets.Max() > maxBinSize){
                 Console.WriteLine("one of the inputs was to big");
                 System.Environment.Exit(0);
@@ -79,6 +34,57 @@ namespace BinPack
             BinPack(maxBinSize, packets);
         }
 
+        private static void BinPack(int maxBinSize, int[] packets)
+        {
+            Dictionary<int, Bin> storage = new Dictionary<int, Bin>();
+            Bin modifedBin = new Bin();
+            Console.WriteLine("Alog starting");
+
+            foreach (int currentPacket in packets)
+            {
+                bool validbin = false;
+
+                for (int i = 1; i <= storage.Count; i++)
+                {
+                    if(storage[i].Space + currentPacket <= maxBinSize){
+                        StringBuilder items = new StringBuilder();
+                        items.Append(storage[i].Items + "," + currentPacket.ToString());
+                        modifedBin.Space = storage[i].Space + currentPacket;
+                        modifedBin.Items = items;
+                        validbin = true;
+                        storage[i] = modifedBin;
+                        break;
+                    }
+                }
+                if(validbin == false){
+                    CreateNewBin(currentPacket, storage);
+                }
+            }
+
+
+            Console.WriteLine("Complete");
+            DisplayItems(storage);
+        }
+        static Dictionary<int, Bin> CreateNewBin(int packet, Dictionary<int, Bin> storage){
+            StringBuilder items = new StringBuilder();
+            items.Append(packet.ToString());
+            int newBinNumber = storage.Count + 1;
+            Bin newBin = new Bin(); newBin.Space = packet;
+            newBin.Items = items;
+            storage.Add(newBinNumber, newBin);
+
+            return storage;
+        }
+
+        static void DisplayItems(Dictionary<int, Bin> storage){
+            long binsUsed = 0;
+            foreach (var item in storage)
+            {
+                Console.WriteLine($"Bin: {item.Key}     Space used: {item.Value.Space}      Items: {item.Value.Items}");
+                binsUsed++;
+            }
+            Console.WriteLine("Total bins: " + binsUsed);
+        }
         static int[] ReadFileData(){
             string packetData = File.ReadAllText("numbers.txt");
             return packetData.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
